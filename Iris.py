72a9b5c5248c5394af10738bcbd62cb3aa350716
prathiktm006-del <prathiktm006@gmail.com>
2026-03-10 17:32:13 +0530

    Create Iris.py

---
+++ Iris.py
@@ -0,0 +1,28 @@
+import sys
+print("Python version: {}".format(sys.version))
+import pandas as pd
+print("pandas version: {}".format(pd.__version__))
+import matplotlib
+print("matplotlib version: {}".format(matplotlib.__version__))
+import numpy as np
+print("NumPy version: {}".format(np.__version__))
+import scipy as sp
+print("SciPy version: {}".format(sp.__version__))
+import IPython
+print("IPython version: {}".format(IPython.__version__))
+import sklearn
+print("scikit-learn version: {}".format(sklearn.__version__))
+from sklearn.datasets import load_iris
+iris_dataset = load_iris()
+print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
+print(iris_dataset['DESCR'][:193] + "\n...")
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Feature names: \n{}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(type(iris_dataset['data'])))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+print("First five columns of data:\n{}".format(iris_dataset['data'][:5]))
+print("Type of target: {}".format(type(iris_dataset['target'])))
+print("Shape of target: {}".format(iris_dataset['target'].shape))
+print("Target:\n{}".format(iris_dataset['target']))
+
+
