88cfe0d3befd5e2134124c638818f66f935a0c94
prathiktm006-del <prathiktm006@gmail.com>
2026-03-14 23:18:46 +0530

    Create lasso.py

---
+++ lasso.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.linear_model import Lasso
+lasso = Lasso().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+# we increase the default setting of "max_iter",
+# otherwise the model would warn us that we should increase max_iter.
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso001.coef_ != 0)))
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso00001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso00001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso00001.coef_ != 0)))
+plt.plot(lasso.coef_, 's', label="Lasso alpha=1")
+plt.plot(lasso001.coef_, '^', label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, 'v', label="Lasso alpha=0.0001")
+plt.plot(ridge01.coef_, 'o', label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
